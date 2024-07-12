bceb87a2d3f9b728baa5cf931112797474590426
whoami <2684533416byj@gmail.com>
2024-07-13 02:43:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# c-learn
+
+参考 C语言程序设计进行回顾，完成 [笨办法学C](https://docs.kilvn.com/lcthw-zh/),同时学习算法以及数据结构。
+
+## 打卡
---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+//
+// Created by hgbe02 on 2024/7/13.
+//
+
+#include <cstdio>
+int main()
+{
+    printf("Hello World!");
+    return 0;
+}
